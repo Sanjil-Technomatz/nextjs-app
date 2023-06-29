a960219b7e1475b214f35985d89c2d521c10d50c
sharad1996 <sharadwankhade23@gmail.com>
2023-06-29 15:50:52 +0530

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function index() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-15">
-      <h1>index</h1>
+      <h1>index testing</h1>
     </main>
   );
 }
